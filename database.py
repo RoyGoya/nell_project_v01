293acd9c18225a74d35c3bc879a34d056fd1f4f2
Roy Goya <roy1goya@gmail.com>
2018-01-10 14:02:16 +0900

    Add database config

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from . import config
+
+engine = create_engine(config.DevelopmentConfig.DATABASE_URI)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+def inint_db():
+    import nell.models.users_model
+    Base.metadata.create_all(bind=engine)
