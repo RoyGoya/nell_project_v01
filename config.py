4aa10591ff72a4fab3733d5fa241591d2b7ae32d
Roy Goya <roy1goya@gmail.com>
2018-01-10 13:59:53 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = ''
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = ''
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DATABASE_URI = 'postgresql://postgres:12344@localhost:5435/nell02_test'
+
+
+class TestingConfig(Config):
+    TESTING = True
---
+++ views/users_view.py
@@ -0,0 +1,69 @@
+from flask import render_template, request, flash, redirect, url_for
+from flask.views import View
+
+from flask_login import login_user, logout_user
+
+from nell.database import db_session
+from nell.forms.users_form import RegistrationForm, LoginForm
+from nell.models.users_model import User
+from nell.helpers.secu_redirect import get_redirect_target, redirect_back
+
+
+class RegisterView(View):
+    methods = ['GET', 'POST']
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def dispatch_request(self):
+        form = RegistrationForm(request.form)
+        if request.method == 'POST' and form.validate():
+            user = User(email=form.email.data,
+                        first_name=form.first_name.data,
+                        last_name=form.last_name.data,
+                        password=form.password.data)
+            db_session.add(user)
+            db_session.commit()
+            flash('Thanks for registering.')
+            login_user(user)
+            flash('Logged in successfully.')
+            return redirect(url_for('profile_page'))
+        return render_template(self.template_name, form=form)
+
+
+class LoginView(View):
+    methods = ['GET', 'POST']
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def dispatch_request(self):
+        form = LoginForm(request.form)
+        if request.method == 'POST' and form.validate():
+            user = User(form.email.data, form.password.data)
+            login_user(user)
+            flash('Logged in successfully.')
+            next = get_redirect_target()
+            return redirect_back(next or 'index_page')
+        return render_template(self.template_name, form=form)
+
+
+class LogoutView(View):
+    methods = ['GET', 'POST']
+
+    def __init__(self, next_url):
+        self.next_url = next_url
+
+    def dispatch_request(self):
+        logout_user()
+        return redirect(url_for(self.next_url))
+
+
+class ProfileView(View):
+    methods = ['GET', 'POST']
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def dispatch_request(self):
+        return render_template(self.template_name)
